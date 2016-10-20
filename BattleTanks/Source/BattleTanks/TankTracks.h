01dd6ad7fa33332c4cba86d28ec079b351f716e8
Fabio Mosca <mosfabioca@gmail.com>
2016-10-20 18:54:57 +0200

    BT 22 Setup Tank Tracks class

---
+++ BattleTanks/Source/BattleTanks/TankTracks.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/StaticMeshComponent.h"
+#include "TankTracks.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BATTLETANKS_API UTankTracks : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
